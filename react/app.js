d60daec06b7414ecf0b69d0f947e3542222db629
lakshminlp <lakshmi.nidadavolu@byjus.com>
2023-05-14 23:01:06 +0530

    feat: :sparkles: react changes step1
    react changes step1

---
+++ react/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PasswordStrengthChecker from "./PasswordStrengthChecker";
+
+function App() {
+  return (
+    <div>
+      <h1>Password Strength Checker</h1>
+      <PasswordStrengthChecker />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/user.js
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>User Profile</title>
+</head>
+<body>
+	<h1>User Profile</h1>
+	<h2>John Doe</h2>
+	<p>Email: johndoe@email.com</p>
+</body>
+</html>
